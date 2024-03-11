2905a508624e172fcb73006390ad00600bdd8116
Shryesth Pandey <shryesth4236@gmail.com>
2024-03-11 16:53:50 +0530

    DOM - query selectors

---
+++ DOM Challenge Starting Files/index.js
@@ -0,0 +1,5 @@
+document.querySelector('ul').lastElementChild.innerHTML = "shryesth";
+
+
+document.getElementsByTagName('a')[0].href = "https://www.instagram.com"
+document.querySelector('.list a').href = "facebook.com"
