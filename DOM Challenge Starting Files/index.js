5019c7ae3b5f08e10f7667cea07242908565c150
Shryesth Pandey <shryesth4236@gmail.com>
2024-03-11 17:30:51 +0530

    toggle and class list

--- DOM Challenge Starting Files/index.js
+++ DOM Challenge Starting Files/index.js
@@ -3,3 +3,9 @@ document.querySelector('ul').lastElementChild.innerHTML = "shryesth";
 
 document.getElementsByTagName('a')[0].href = "https://www.instagram.com"
 document.querySelector('.list a').href = "facebook.com"
+
+
+// document.querySelector("h1").style.fontSize = '1rem';
+
+
+document.querySelector("button").style.backgroundColor = 'green';
